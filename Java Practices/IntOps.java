f285e6bfd1943e008a1fb019a66c9f1319aeac2f
Sasitha Lakshan <74831645+SasithaLakshan@users.noreply.github.com>
2021-07-18 15:34:40 +0530

    Integer Operations

---
+++ Java Practices/IntOps.java
@@ -0,0 +1,22 @@
+/*
+Illustrates the integer operations a + b, a * b, a / b, and a % b.
+Coded by : Sasitha
+*/
+
+public class IntOps{
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        //Operations
+        int sum = a + b;
+        int prod = a * b;
+        int quot = a / b;
+        int rem = a % b;
+
+        System.out.println(a + " + " + b + " = " + sum);
+        System.out.println(a + " * " + b + " = " + prod);
+        System.out.println(a + " / " + b + " = " + quot);
+        System.out.println(a + " % " + b + " = " + rem);
+    }
+}
