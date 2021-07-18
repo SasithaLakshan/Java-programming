477bac13e1345c42207b2c0ca3610da882fed100
Sasitha Lakshan <74831645+SasithaLakshan@users.noreply.github.com>
2021-07-18 14:38:42 +0530

    Characters

--- Java Practices/characters.java
+++ Java Practices/characters.java
@@ -8,12 +8,16 @@ public class characters {
        
        char [] FirstName = {'S','A','S','I','T','H','A'};
        char [] MiddleName = {'L','A','K','S','H','A','N'};
+       char [] SurName = {'K','U','M','A','R','A'};
 
        for(char c:FirstName){
            System.out.println(c);
        }
        for(char d:MiddleName){
            System.out.println(d);
+       }
+       for(char e:SurName){
+           System.out.println(e);
        }
     }
 }
