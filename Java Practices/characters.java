fac52c45009cc4dd1edcaf9cffbd31f156f3401e
Sasitha Lakshan <74831645+SasithaLakshan@users.noreply.github.com>
2021-07-18 14:34:22 +0530

    Strings

---
+++ Java Practices/characters.java
@@ -0,0 +1,19 @@
+/*
+Strings in JAVA
+Coded by : Sasitha
+*/
+
+public class characters {
+    public static void main(String[] args){
+       
+       char [] FirstName = {'S','A','S','I','T','H','A'};
+       char [] MiddleName = {'L','A','K','S','H','A','N'};
+
+       for(char c:FirstName){
+           System.out.println(c);
+       }
+       for(char d:MiddleName){
+           System.out.println(d);
+       }
+    }
+}
---
+++ Java Practices/strings.java
@@ -0,0 +1,15 @@
+/*
+Strings in JAVA
+Coded by : Sasitha
+*/
+
+public class strings{
+    public static void main(String[] args){
+        String a, b, c;
+        a = "Hello ";
+        b = "Sasitha";
+        c = a + b;
+
+        System.out.println(c + ", you're enjoying this right? GREATTTT !!");
+    }
+}
