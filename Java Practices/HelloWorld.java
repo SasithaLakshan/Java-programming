e61335c690400867e059d5b66af7ba2f07979245
Sasitha Lakshan <74831645+SasithaLakshan@users.noreply.github.com>
2021-07-18 13:18:38 +0530

    Create HelloWorld.java

---
+++ Java Practices/HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+First JAVA Program that writes name and some other stuffs
+Coded by : Sasitha
+*/
+
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello, I'm Sasitha \nKind of mysterious guy :) \nThis is the First JAVA Program I ever wrote");
+    }
+}
